5caf5b3ae650c71976f995a9b55aad44c3340359
technohorizons <gaurav.v@technocratshorizons.com>
2016-12-13 04:34:46 +0530

    Initil Commit

--- app/Modules/Review/Models/Review.php
+++
@@ -1,8 +0,0 @@
-<?php namespace App\Modules\Review\Models;
-use Illuminate\Database\Eloquent\Model;
-
-class Review extends Model
-{
-    protected $table = 'user_review';
-    protected $guarded = array('id');
-}
